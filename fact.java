bb2c6271311778b737badbcb63b24066f7b265d5
Srinivedha <srinivedhaperiyasamy@gmail.com>
2018-06-28 21:35:44 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+class fact
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if((a==0)||(a==1))
+        {
+            System.out.print("1");
+        }
+        else
+        {
+        int mul=1;
+        for(int i=1;i<=a;i++)
+        {
+            mul=mul*i;
+        }
+        System.out.print(mul);
+        }
+    }
+}
